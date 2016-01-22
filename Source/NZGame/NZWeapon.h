89abb4cf29128f6d4d961add39ac00ab43a2c959
terryzhong <terryzhong@tencent.com>
2016-01-23 03:42:00 +0800

    Modify.

--- Source/NZGame/NZWeapon.h
+++ Source/NZGame/NZWeapon.h
@@ -34,27 +34,6 @@ struct FInstantHitDamageInfo
 };
 
 USTRUCT()
-struct FDelayedProjectileInfo
-{
-    GENERATED_USTRUCT_BODY()
-    
-    UPROPERTY()
-    TSubclassOf<class ANZProjectile> ProjectileClass;
-    
-    UPROPERTY()
-    FVector SpawnLocation;
-    
-    UPROPERTY()
-    FRotator SpawnRotation;
-    
-    FDelayedProjectileInfo()
-        : ProjectileClass(NULL)
-        , SpawnLocation(ForceInit)
-        , SpawnRotation(ForceInit)
-    {}
-};
-
-USTRUCT()
 struct FDelayedHitScanInfo
 {
     GENERATED_USTRUCT_BODY()
@@ -72,11 +51,38 @@ struct FDelayedHitScanInfo
     FRotator SpawnRotation;
     
     FDelayedHitScanInfo()
-        : ImpactLocation(ForceInit)
-        , FireMode(0)
-        , SpawnLocation(ForceInit)
-        , SpawnRotation(ForceInit)
+    : ImpactLocation(ForceInit)
+    , FireMode(0)
+    , SpawnLocation(ForceInit)
+    , SpawnRotation(ForceInit)
     {}
+};
+
+UENUM(BlueprintType)
+namespace EZoomState
+{
+    enum Type
+    {
+        EZS_NotZoomed,
+        EZS_ZoomingIn,
+        EZS_ZoomingOut,
+        EZS_Zoomed,
+    };
+}
+
+USTRUCT(BlueprintType)
+struct FZoomInfo
+{
+    GENERATED_USTRUCT_BODY()
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Zoom)
+    float StartFOV;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Zoom)
+    float EndFOV;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Zoom)
+    float Time;
 };
 
 /**
@@ -592,19 +598,338 @@ public:
 
 	virtual void PlayImpactEffects(const FVector& TargetLoc, uint8 FireMode, const FVector& SpawnLocation, const FRotator& SpawnRotation);
 
+    /** Shared code between NZWeapon and NZWeaponAttachment to refine replicated FlashLocation into impact effect transform via trace */
 	static FHitResult GetImpactEffectHit(APawn* Shooter, const FVector& StartLoc, const FVector& TargetLoc);
 
-
-
+    /** Return start point for weapons fire */
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = Weapon)
+    virtual FVector GetFireStartLoc(uint8 FireMode = 255);
     
+    /** Return base fire direction for weapons fire (i.e. direction player's weapon is pointing) */
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = Weapon)
+    virtual FRotator GetBaseFireRotation();
+    
+    /** 
+     * Return adjusted fire rotation after accounting for spread, aim help, and any other secondary factors affecting aim direction
+     * (may include randomized components)
+     */
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, BlueprintPure, Category = Weapon)
+    FRotator GetAdjustedAim(FVector StartFireLoc);
+    
+    /** If owned by a human, set ANZPlayerController::LastShotTargetGuess to closest target to player's aim */
+    UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = AI)
+    void GuessPlayerTarget(const FVector& StartFireLoc, const FVector& FireDir);
+    
+    UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = Weapon)
+    virtual void AddAmmo(int32 Amount);
+    
+    UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = Weapon)
+    virtual void ConsumeAmmo(uint8 FireModeNum);
+    
+    virtual void FireInstantHit(bool bDealDamage = true, FHitResult* OutHit = NULL);
+    
+    UFUNCTION(BlueprintCallable, Category = Firing)
+    void K2_FireInstantHit(bool bDealDamage, FHitResult& OutHit);
+    
+    /** Handles rewind functionality for net games with ping prediction */
+    virtual void HitScanTrace(const FVector& StartLocation, const FVector& EndTrace, float TraceRadius, FHitResult& Hit, float PredictionTime);
+    
+    UFUNCTION(BlueprintCallable, Category = Firing)
+    virtual ANZProjectile* FireProjectile();
+    
+    /** Spawn a projection on both server and owning client, and forward predict it by 1/2 ping on server */
+    virtual ANZProjectile* SpawnNetPredictedProjectile(TSubclassOf<ANZProjectile> ProjectileClass, FVector SpawnLocation, FRotator SpawnRotation);
+    
+    /** Returns whether we can meet AmmoCost for the given fire mode */
+    UFUNCTION(BlueprintCallable, Category = Weapon)
+    virtual bool HasAmmo(uint8 FireModeNum);
+    
+    /** Returns whether we have ammo for any fire mode */
     UFUNCTION(BlueprintCallable, Category = Weapon)
     virtual bool HasAnyAmmo();
     
+    /** Get interval between shots, including any fire rate modifiers */
+    UFUNCTION(BlueprintCallable, Category = Weapon)
+    virtual float GetRefireTime(uint8 FireModeNum);
     
+    inline uint8 GetCurrentFireMode()
+    {
+        return CurrentFireMode;
+    }
     
+    inline void GotoActiveState()
+    {
+        GotoState(ActiveState);
+    }
     
+    virtual void GotoFireMode(uint8 NewFireMode);
     
+    UFUNCTION(Blueprintcallable, Category = Weapon)
+    bool IsFiring() const;
     
+    virtual bool StackPickup_Implementation(ANZInventory* ContainedInv) override;
     
+    /** Update any timers and such due to a weapon timing change; for example, a powerup that changes firing speed */
+    virtual void UpdateTiming();
     
+    virtual void Tick(float DeltaTime) override;
+    
+    virtual void Destroyed() override;
+    
+    static void InstanceMuzzleFlashArray(AActor* Weap, TArray<UParticleSystemComponent*>& MFArray);
+    
+    inline UNZWeaponState* GetCurrentState()
+    {
+        return CurrentState;
+    }
+    
+    /** 
+     * Called on clients only when the local owner got a kill while holding this weapon
+     * NOTE: this weapon didn't necessarily cause the kill (previously fired projecctile, etc), if you care check the damagetype
+     */
+    UFUNCTION(BlueprintNativeEvent, BlueprintCosmetic)
+    void NotifyKillWhileHolding(TSubclassOf<UDamageType> DmgType);
+    
+    virtual float GetHeadshotScale() const
+    {
+        return 0.0f;
+    }
+    
+    /** Return true if needs HUD ammo display widget drawn */
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, BlueprintCosmetic, Category = HUD)
+    bool NeedsAmmoDisplay() const;
+    
+    /** Returns crosshair color taking into account user settings, red flash on hit, etc */
+    UFUNCTION(BlueprintCallable, Category = Weapon)
+    FLinearColor GetCrosshairColor(class UNZHUDWidget* WeaponHudWidget) const;
+    
+    /** The player state of the player currently under the crosshair */
+    UPROPERTY()
+    ANZPlayerState* TargetPlayerState;
+    
+    /** The time this player was last seen under the crosshair */
+    float TargetLastSeenTime;
+    
+    /** Returns whether we should draw the friendly fire indicator on the crosshair */
+    UFUNCTION(BlueprintCallable, Category = Weapon)
+    virtual bool ShouldDrawFFIndicator(APlayerController* Viewer, ANZPlayerState*& HitPlayerState) const;
+    
+    /** Returns desired crosshair scale (affected by recent pickups) */
+    UFUNCTION(BlueprintCallable, Category = Weapon)
+    virtual float GetCrosshairScale(class ANZHUD* HUD);
+    
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = HUD)
+    void DrawWeaponCrosshair(UNZHUDWidget* WeaponHudWidget, float RenderDelta);
+    
+    UFUNCTION()
+    void UpdateCrosshairTarget(ANZPlayerState* NewCrosshairTarget, UNZHUDWidget* WeaponHudWidget, float RenderDelta);
+    
+    /**
+     * Default parameters set on overlay particle effect (if any)
+     * Up to the effect to care about them
+     */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Effects)
+    TArray<struct FParticleSysParam> OverlayEffectParams;
+    
+    /**
+     * Helper for shared overlay code between NZWeapon and NZWeaponAttachment
+     * NOTE: can called on default object!
+     */
+    virtual void UpdateOverlaysShared(AActor* WeaponActor, ANZCharacter* InOwner, USkeletalMeshComponent* InMesh, const TArray<struct FParticleSysParam>& InOverlayEffectParams, USkeletalMeshComponent*& InOverlayMesh) const;
+    /** Read WeaponOverlayFlags from owner and apply the appropriate overlay material (if any) */
+    virtual void UpdateOverlays();
+    
+    virtual void SetSkin(UMaterialInterface* NewSkin);
+    
+    UPROPERTY()
+    FRotator LastRotation;
+    
+    UPROPERTY()
+    float OldRotDiff[2];
+    UPROPERTY()
+    float OldLeadMag[2];
+    UPROPERTY()
+    float OldMaxDiff[2];
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon Rotation)
+    float RotChgSpeed;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon Rotation)
+    float ReturnChgSpeed;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon Rotation)
+    float MaxYawLag;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon Rotation)
+    float MaxPitchLag;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon Rotation)
+    bool bProceduralLagRotation;
+    
+    virtual bool ShouldLagRot();
+    
+    virtual float LagWeaponRotation(float NewValue, float LastValue, float DeltaTime, float MaxDiff, int32 Index);
+    
+    virtual void UpdateWeaponHand();
+    
+    EWeaponHand GetWeaponHand() const;
+    
+    virtual void UnEquip();
+    
+    virtual void GotoEquippingState(float OverflowTime);
+    
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = Weapon)
+    virtual bool IsUnEquipping() { return GetCurrentState() == UnequippingState; }
+    
+    UFUNCTION(BlueprintNativeEvent, Category = AI)
+    float GetDamageRadius(uint8 TestMode) const;
+    
+    virtual float BotDesireability_Implementation(APawn* Asker, AActor* Pickup, float PathDistance) const;
+    virtual float DetourWeight_Implementation(APawn* Asker, AActor* Pickup, float PathDistance) const;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
+    float BaseAISelectRating;
+    
+    UFUNCTION(BlueprintNativeEvent, Category = AI)
+    float GetAISelectRating();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = AI)
+    float SuggestAttackStyle();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = AI)
+    float SuggestDefenseStyle();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = AI)
+    bool IsPreparingAttack();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = AI)
+    bool ShouldAIDelayFiring();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = AI)
+    bool CanAttack(AActor* Target, const FVector& TargetLoc, bool bDirectOnly, bool bPreferCurrentMode, UPARAM(ref) uint8& BestFireMode, UPARAM(ref) FVector& OptimalTargetLoc);
+    
+    inline bool CanAttack(AActor* Target, const FVector& TargetLoc, bool bDirectOnly)
+    {
+        uint8 UnusedFireMode;
+        FVector UnusedOptimalLoc;
+        return CanAttack(Target, TargetLoc, bDirectOnly, false, UnusedFireMode, UnusedOptimalLoc);
+    }
+    
+    virtual bool DoAssistedJump()
+    {
+        return false;
+    }
+    
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Mesh)
+    TArray<UMeshComponent*> Get1PMeshes() const;
+    
+protected:
+    UPROPERTY(BlueprintReadOnly, Category = Weapon)
+    UNZWeaponState* CurrentState;
+    
+    UPROPERTY(BlueprintReadOnly, Category = Weapon)
+    uint8 CurrentFireMode;
+    
+    UPROPERTY(Instanced, BlueprintReadOnly, Category = States)
+    UNZWeaponState* ActiveState;
+    
+    UPROPERTY(Instanced, BlueprintReadOnly, Category = States)
+    class UNZWeaponStateEquipping* EquippingState;
+    
+    UPROPERTY(Instanced, BlueprintReadOnly, Category = States)
+    UNZWeaponState* UnequippingStateDefault;
+    
+    UPROPERTY(Instanced, BlueprintReadOnly, Category = States)
+    UNZWeaponState* UnequippingState;
+    
+    UPROPERTY(Instanced, BlueprintReadOnly, Category = States)
+    UNZWeaponState* InactiveState;
+    
+    UPROPERTY()
+    USkeletalMeshComponent* OverlayMesh;
+    
+public:
+    float WeaponBarScale;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = HUD)
+    FTextureUVs WeaponBarSelectedUVs;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = HUD)
+    FTextureUVs WeaponBarInactiveUVs;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+    FName KillStatsName;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+    FName AltKillStatsName;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+    FName DeathStatsName;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+    FName AltDeathStatsName;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+    FName ShotsStatsName;
+    
+    virtual int32 GetWeaponKillStats(ANZPlayerState* PS) const;
+    virtual int32 GetWeaponDeathStats(ANZPlayerState* PS) const;
+    virtual float GetWeaponShotsStats(ANZPlayerState* PS) const;
+    virtual float GetWeaponHitsStats(ANZPlayerState* PS) const;
+    
+    UFUNCTION(exec)
+    void TestWeaponLoc(float X, float Y, float Z);
+    UFUNCTION(exec)
+    void TestWeaponRot(float Pitch, float Yaw, float Roll = 0.0f);
+    UFUNCTION(exec)
+    void TestWeaponScale(float X, float Y, float Z);
+    
+    UFUNCTION(BlueprintImplementableEvent, Category = Weapon)
+    void NotifyTeamChanged();
+    
+    UFUNCTION(BlueprintNativeEvent, Category = Weapon)
+    void FiringInfoUpdated(uint8 InFireMode, uint8 FlashCount, FVector InFlashLocation);
+    UFUNCTION(BlueprintNativeEvent, Category = Weapon)
+    void FiringExtraUpdated(uint8 NewFlashExtra, uint8 InFireMode);
+    UFUNCTION(BlueprintNativeEvent, Category = Weapon)
+    void FiringEffectsUpdated(uint8 InFireMode, FVector InFlashLocation);
+    
+    UPROPERTY(Replicated, ReplicatedUsing = OnRep_ZoomCount)
+    uint8 ZoomCount;
+    UFUNCTION()
+    virtual void OnRep_ZoomCount();
+    
+    UPROPERTY(BlueprintReadOnly, Category = Zoom, Replicated, ReplicatedUsing = OnRep_ZoomState)
+    TEnumAsByte<EZoomState::Type> ZoomState;
+    UFUNCTION(BlueprintNativeEvent)
+    void OnRep_ZoomState();
+    
+    UPROPERTY(BlueprintReadOnly, Category = Zoom, Replicated)
+    uint8 CurrentZoomMode;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Zoom)
+    TArray<FZoomInfo> ZoomModes;
+    
+    UPROPERTY(BlueprintReadOnly, Category = Zoom, Replicated)
+    float ZoomTime;
+    
+    UFUNCTION(BlueprintCallable, Category = Zoom)
+    virtual void SetZoomMode(uint8 NewZoomMode);
+    UFUNCTION(Server, Reliable, WithValidation)
+    virtual void ServerSetZoomMode(uint8 NewZoomMode);
+    virtual void LocalSetZoomMode(uint8 NewZoomMode);
+    
+    UFUNCTION(BlueprintCallable, Category = Zoom)
+    virtual void SetZoomState(TEnumAsByte<EZoomState::Type> NewZoomState);
+    UFUNCTION(Server, Reliable, WithValidation)
+    virtual void ServerSetZoomState(uint8 NewZoomState);
+    virtual void LocalSetZoomState(uint8 NewZoomState);
+    
+    UFUNCTION(BlueprintNativeEvent)
+    void OnZoomedIn();
+    
+    UFUNCTION(BlueprintNativeEvent)
+    void OnZoomedOut();
+    
+    virtual void TickZoom(float DeltaTime);
 };
